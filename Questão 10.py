2ee5262ac1bd47a15777e1e36cb28a24f9b53436
JesusCaua10 <166930440+JesusCaua10@users.noreply.github.com>
2024-06-10 15:14:43 -0300

    Update Questão 10.py

--- Questão 10.py
+++ Questão 10.py
@@ -1,5 +1,5 @@
 vr_num = int(input("Diga um numero para ver sua tabuada: "))
 print(f"tabuada do {vr_num}")
-for num in range(1,10):
+for num in range(1,11):
     res = vr_num * num
     print(f"{vr_num} x {num} = {res} ")
