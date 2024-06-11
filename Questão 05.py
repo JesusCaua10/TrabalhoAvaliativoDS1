7c0b3ec69f2dbb13fe714054b468f108f68946d6
JesusCaua10 <166930440+JesusCaua10@users.noreply.github.com>
2024-06-11 10:57:36 -0300

    Update Questão 05.py

--- Questão 05.py
+++ Questão 05.py
@@ -1,9 +1,9 @@
 vr_hora = int(input("Diga uma hora entre 0 e 23: "))
-if 1 < vr_hora < 12 :
+if 0 <= vr_hora < 12 :
   print("Está de manhã")
-elif 11 < vr_hora < 18 :
+elif 12 <= vr_hora < 18 :
   print("Está de tarde")
-elif 17 < vr_hora < 24 :
+elif 18 <= vr_hora < 24 :
   print("Está de noite")
 else :
   print("Valor inválido")
